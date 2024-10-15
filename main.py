32233353c343c1e3ae3d466cd07b4f67941db384
James Bridge <naetharu@gmail.com>
2024-10-15 14:26:22 +0100

    Move player object creation to after group creation

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from player import Player
 
 def main():
     pygame.init()
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     clock = pygame.time.Clock()
